73d9775a2b904e98adcae74ee0eb947b6057e6e9
lypsoftware2016 <1024164331@qq.com>
2019-11-12 01:03:52 +0800

    实现报警列表前后端交互

--- front/whp/pages/admin/alarmDetail/alarmDetail.js
+++ front/whp/pages/admin/alarmDetail/alarmDetail.js
@@ -5,14 +5,57 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    alarmDetail: undefined,
+    position: {
+      longitude: '',
+      latitude: ''
+    },
+    markers: [{
+      iconPath: "/resources/others.png",
+      id: 0,
+      latitude: 23.099994,
+      longitude: 113.324520,
+      width: 50,
+      height: 50
+    }],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var that = this
+    wx.request({
+      url: 'http://120.55.54.247:8090/alarm/getDetail',
+      data: { alarmId: options.alarmId },
+      success: function (res) {
+        if (res.data.code != 0) {
+          wx.showToast({
+            title: '获取数据失败',
+          })
+        }
+        else {
+          var pos = res.data.data.position.split(",")
+          that.setData({
+            alarmDetail: res.data.data,
+            position: {
+              longitude: pos[0],
+              latitude: pos[1]
+            },
+          });
+        }
+      }
+    })
+  },
 
+  locate: function () {
+    var that = this
+    var longitude = parseFloat(that.data.position.longitude)
+    var latitude = parseFloat(that.data.position.latitude)
+    wx.openLocation({
+      latitude: latitude, // 纬度，范围为-90~90，负数表示南纬
+      longitude: longitude, // 经度，范围为-180~180，负数表示西经
+    })
   },
 
   /**
--- front/whp/pages/admin/alarmList/alarmList.js
+++ front/whp/pages/admin/alarmList/alarmList.js
@@ -3,16 +3,28 @@ Page({
 
   /**
    * 页面的初始数据
+   * time < 60s 1分钟前
+   * 60s < time < 3600s time/60 分钟前
+   * 3600s <= time < 86400s time/3600 小时前
+   * 86400s <= time < 31536000s time/86400 天前
+   * 31536000 <= time time/31536000 年前
    */
   data: {
-
+    alarmList: undefined,
+    now: undefined
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+  },
 
+  alarmDetail: function (e) {
+    var id = e.currentTarget.dataset.id;
+    wx.navigateTo({
+      url: '/pages/admin/alarmDetail/alarmDetail?alarmId=' + id,
+    })
   },
 
   /**
@@ -26,7 +38,33 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    var that = this;
+    var timestamp = Date.parse(new Date());
+    timestamp = timestamp / 1000;
+    that.setData({
+      now: timestamp
+    })
+    console.log("当前时间戳为：" + that.data.now);
+    wx.request({
+      url: 'http://120.55.54.247:8090/alarm/getList',
+      method: 'get',
+      success: function (res) {
+        if (res.data.code != 0) {
+          wx.showToast({
+            title: '获取数据失败',
+          })
+        } else {
+          that.setData({
+            alarmList: res.data.data,
+            length: res.data.data.length
+          })
+        }
+        console.log(that.data.alarmList)
+      },
+      fail: function (err) {
+        console.log(err)
+      }
+    })
   },
 
   /**
