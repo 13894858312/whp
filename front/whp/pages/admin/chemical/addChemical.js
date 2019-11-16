9df3567611ac71e2d58e38a87c35917c15a03520
151250149 <151250149@smail.nju.edu.cn>
2019-11-16 21:36:44 +0800

    引入weui.wxss，添加化学品静态页面

--- front/whp/pages/admin/chemical/addChemical.js
+++ front/whp/pages/admin/chemical/addChemical.js
@@ -1,66 +1,21 @@
 // pages/admin/chemical/addChemical.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  addChemical: function(e){
+    var data = e.detail.value;
+    if(data.cas == ''){
+      wx.showToast({
+        title: '请输入化学品CAS号',
+        icon: 'none'
+      })
+      return;
+    }
+    if(data.cnName == ''){
+      wx.showToast({
+        title: '请输入化学品中文名称',
+        icon: 'none'
+      })
+      return;
+    }
+  
   }
 })
