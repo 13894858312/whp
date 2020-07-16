2e1e0d860da705663a83b212eb41b4afb58debe3
刘一澎 <1024164331@qq.com>
2020-07-16 15:09:21 +0800

    完善删除功能

--- front/whp/pages/admin/alarmList/alarmList.js
+++ front/whp/pages/admin/alarmList/alarmList.js
@@ -23,8 +23,7 @@ Page({
     var that = this;
     var alarmList = that.data.alarmList;
     var index = e.currentTarget.dataset.index;//获取当前长按图片下标
-    var id = e.currentTarget.dataset.id;
-    console.log(id)
+    var alarmId = e.currentTarget.dataset.id;
     wx.showModal({
      title: '提示',
      content: '确定要删除此报警吗？',
@@ -32,10 +31,28 @@ Page({
       if (res.confirm) {
        console.log('点击确定了');
        alarmList.splice(index, 1);
-       /**
-        * 删除订单
-        * 
-        *  */ 
+       wx.request({
+        url: 'https://chem.ufeng.top/whp/alarm/del',
+        method: 'POST',
+        header: {
+          'content-type': 'application/x-www-form-urlencoded'
+        },
+        data: {alarmId: alarmId},
+        success: function (res) {
+          if (res.data.code != 0) {
+            wx.showToast({
+              title: '删除报警失败',
+            })
+          } else {
+            wx.showToast({
+              title: '删除报警成功',
+            })
+          }
+        },
+        fail: function (err) {
+          console.log(err)
+        }
+      })
       } else if (res.cancel) {
         console.log('点击取消了');
         return false;    
